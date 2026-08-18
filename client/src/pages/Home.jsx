77cfc5b32e6546ed9c75c9552b383fe87cbc9413
Soham Ghosh <ghoshsoham173@gmail.com>
2026-08-19 02:37:54 +0530

    Fix caretaker card import casing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { api } from "../api/axios.js";
-import CaretakerCard from "../components/CaretakerCard.jsx";
+import CaretakerCard from "../components/careTakerCard.jsx";
 import FilterBar from "../components/FilterBar.jsx";
 import { useAuth } from "../context/AuthContext.jsx";
 
